f24eb0a24af5c0ad2b2b1f99a8d8bff736e5c1bb
JaneBabiakA <99457356+JaneBabiakA@users.noreply.github.com>
2022-03-17 20:40:45 -0400

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,15 +1,12 @@
 import tkinter as tk
 from tkinter import *
+import math
 
 
-def lexer(form_input, mystring, x):  # make this work with negative numbers
+def lexer(form_input, mystring, x):
     mystring = [j for j in mystring]
     to_add = []
     while len(mystring) > 0:
-        try:
-            mystring[0] = int(mystring[0])
-        except:  # i could us an isalpha or isnumeric here, maybe a later problem ?
-            pass
         if mystring[0] == "(":
             mystring.pop(0)
             myfunction = lexer([], mystring, x)
@@ -19,33 +16,47 @@ def lexer(form_input, mystring, x):  # make this work with negative numbers
             form_input.append(to_add)
             mystring.pop(0)
             return form_input, mystring
-        elif mystring[0] in ["+", "-", "*", "/"]:
+        elif mystring[0] in ["+", "-", "*", "/", "^"]:
             if to_add:
-                form_input.append("".join(to_add))
-            to_add = []
-            form_input.append(mystring[0])
+                form_input.append(to_add)
+                to_add = []
+            if mystring[0] == "-" and len(form_input) == 0:
+                to_add.append(mystring[0]) # TODO: resolve this problemm!!! <3 input inf
+            else:
+                if mystring[1] == "-":
+                    to_add.append(mystring[1])
+                    mystring.pop(1)
+                else:
+                    form_input.append(mystring[0])
             mystring.pop(0)
-        elif mystring[0] == "^":
+        elif mystring[0] in ["c", "s", "t"]:
             if to_add:
-                form_input[len(form_input)] = "".join(to_add) * pow(form_input, mystring[1])
+                form_input.append(to_add)
+                to_add = []
+            mystring.pop(1)
+            thingu = lexer([], mystring[1::], x)
+            form_input.append([mystring[0], thingu[0]])
             mystring.pop(0)
-            mystring.pop(0)
+            mystring = thingu[1]
         else:
             if mystring[0] == "x":
                 if to_add:
-                    print("".join(to_add), x)
-                    to_add = ["".join(to_add) * x]
+                    to_add = ["".join(to_add), "*"]
+                to_add.append(str(x))
             else:
                 to_add.append(str(mystring[0]))
             mystring.pop(0)
     if to_add:
-        form_input.append("".join(to_add))
+        if "*" in to_add or "^" in to_add:
+            form_input.append(to_add)
+        else:
+            form_input.append("".join(to_add))
     return form_input
 
 
 def infinity(mylist, x, left_hand, right_hand):
     if right_hand and left_hand:
-        if infinity(mylist, x, True, False) == infinity(mylist, x, False, True):  # TODO: switch these to variables
+        if infinity(mylist, x, True, False) == infinity(mylist, x, False, True):
             return infinity(mylist, x, True, False)
         else:
             return "DNE"
@@ -55,17 +66,17 @@ def infinity(mylist, x, left_hand, right_hand):
         x -= 0.001
     limit = plug(mylist[::])
     if limit > 0:
-        print("voila", limit)
         return 'INF'
     else:
-        print("voilall", limit)
         return "NINF"
 
 
 def plug(mylist):
     if type(mylist) == list:
-        print(mylist)
-        result = float(plug(mylist[0]))
+        if mylist[0] not in ["c", "s", "t"]:
+            result = float(plug(mylist[0]))
+        else:
+            pass
     else:
         result = mylist
         mylist = [mylist]
@@ -73,27 +84,39 @@ def plug(mylist):
         if mylist[j] == "+":
             result += float(plug(mylist[j + 1]))
         elif mylist[j] == "-":
-            result -= float(plug(mylist[j + 1]))
+            if j == 0:
+                pass
+            #elif type(mylist[j]) != float:
+                #result = 0 - float(plug(mylist[j + 1]))
+            else:
+                result -= float(plug(mylist[j + 1]))
         elif mylist[j] == "*":
-            print("before", result)
             result = result * float(plug(mylist[j + 1]))
-            print("after", result)
         elif mylist[j] == "/":
             dvdby = float(plug(mylist[j + 1]))
             result = result / dvdby
+        elif mylist[j] == "^":
+            result = pow(float(plug(mylist[j - 1])), float(plug(mylist[j + 1])))
+        elif mylist[j] == "c":
+            result = math.cos(float(plug(mylist[j + 1])))
+        elif mylist[j] == "s":
+            result = math.sin(float(plug(mylist[j + 1])))
+        elif mylist[j] == "t":
+            result = math.tan(float(plug(mylist[j + 1])))
         elif type(mylist[0]) == int:
             result = mylist[0]
+    print(mylist, result)
     return result
 
 
 def inputList(input, equation_input):
-    if input == "DEL":
+    if input == "D":
         if len(equation_input) > 0:
             equation_input.pop()
-    elif input == "=":  # if 1, then True, if 0, then False
+    elif input == "=":
         l_hand = left_hand.get()
         r_hand = right_hand.get()
-        lim = int(limEntry.get())
+        lim = float(limEntry.get())
         if l_hand == 1 and r_hand == 1:
             left_result = myround(plug(lexer([], "".join(equation_input), lim - 0.00001)))
             right_result = myround(plug(lexer([], "".join(equation_input), lim + 0.00001)))
@@ -114,18 +137,16 @@ def inputList(input, equation_input):
 
 
 def myround(number):
-    print("hi")
     if number >= 1000:
         number = "INF"
     elif number <= -1000:
         number = "NINF"
     else:
-        print("hi")
         number = round(number, 2)
     return number
 
 
-# TODO: if DNE, cos, tan, sin, cot, csc, sec, x^2, sqrt, log/ln
+# TODO: if log/ln, negative numbers
 
 
 equation_input = []
@@ -159,7 +180,7 @@ row = -1
 for i in range(len(buttons)):
     if i % 5 == 0:
         row += 1
-    column = (i) % 5
+    column = i % 5
     newButton = tk.Button(text=buttons[i], master=buttonFrame, relief=tk.RAISED, borderwidth=3)
     newButton.grid(column=column, row=row)
     newButton.bind("<Button-1>", lambda e, i=i: inputList(buttons[i], equation_input))
